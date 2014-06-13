1924eebe20be93404cf453791a4f03fd2d0b05ac
Zack Liston <zackmliston@gmail.com>
2014-06-12 19:51:01 -0700

    Initial Commit

---
+++ BooksBooksBooks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BooksBooksBooks
+//
+//  Created by Zack Liston on 6/12/14.
+//  Copyright (c) 2014 zackliston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ BooksBooksBooksTests/BooksBooksBooksTests.m
@@ -0,0 +1,39 @@
+//
+//  BooksBooksBooksTests.m
+//  BooksBooksBooksTests
+//
+//  Created by Zack Liston on 6/12/14.
+//  Copyright (c) 2014 zackliston. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BooksBooksBooksTests : XCTestCase
+
+@end
+
+@implementation BooksBooksBooksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
